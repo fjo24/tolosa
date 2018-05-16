d15957baa35b3fc614ea27320e0c7fed1197718d
fjo24 <fjo224@gmail.com>
2018-05-15 23:16:39 -0300

    header ready

--- resources/views/pages/templates/header.blade.php
+++ resources/views/pages/templates/header.blade.php
@@ -1,33 +1,23 @@
 <header>
 <nav class="header-nav">
-  <div class="nav-wrapper">
-
+  <div class="container nav-flex">
           <a href="" class="logo-header"><img class="responsive-img logo" src="{{asset('img/logo/logo-header.png')}}" alt=""></a>
           <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons li-ppal">menu</i></a>
-          <div class="cont">
           <div class="right hide-on-med-and-down">
-          <div class="redes-header">
-            <a href="" class="iconos-redes"><img src="{{asset('img/redes/lupa.png')}}"></a>
-            <a class="dropdown-button" href="#!" data-activates="dropdown">{{ Lang::get('general.productos') }}</a>
-            <a href="" class="iconos-redes"><img src="{{asset('img/redes/mail.png')}}"></a>
-            <a href="" class="iconos-redes"><img src="{{asset('img/redes/facebook.png')}}"></a>
-            <a href="" class="iconos-redes"><img src="{{asset('img/redes/instagram.png')}}"></a>
-          </div>
-          
-          <div class="links-header">
-            <div class="row">
-
-              <div class="col-l2">
-                <a href="" > EMPRESA  |   </a>
-                <a href="" > PRODUCTOS   |   </a>
-                <a href="" > SERVICIOS   |  </a>
-                <a href="" > OBRA  |   </a>
-                <a href="" > FÁBRICA   |   </a>
-                <a href="" > SOLICITUD DE PRESUPUESTO</a> 
-              </div>
+            <div class="redes-header">
+              <a href="" class="iconos-redes"><img src="{{asset('img/redes/lupa.png')}}"></a>
+              <a href="" class="iconos-redes"><img src="{{asset('img/redes/mail.png')}}"></a>
+              <a href="" class="iconos-redes"><img src="{{asset('img/redes/facebook.png')}}"></a>
+              <a href="" class="iconos-redes"><img src="{{asset('img/redes/instagram.png')}}"></a>
+            </div>      
+            <div class="links-header" style="width: 1200px">
+                  <a href="" > EMPRESA  |   </a>
+                  <a href="" > PRODUCTOS   |   </a>
+                  <a href="" > SERVICIOS   |  </a>
+                  <a href="" > OBRA  |   </a>
+                  <a href="" > FÁBRICA   |   </a>
+                  <a href="" > SOLICITUD DE PRESUPUESTO</a> 
             </div>
-          </div>
-          </div>
           </div>
           <ul class="side-nav" id="mobile-demo">
             <a href="" class="iconos-redes"><img src="{{asset('img/redes/lupa.png')}}"></a>
@@ -35,6 +25,6 @@
             <a href="" class="iconos-redes"><img src="{{asset('img/redes/facebook.png')}}"></a>
             <a href="" class="iconos-redes"><img src="{{asset('img/redes/instagram.png')}}"></a>
           </ul>
-  </nav>
+  </div>
 </nav>
 </header>
